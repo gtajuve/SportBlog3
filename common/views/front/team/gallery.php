3327b17442441822ede65009cf730411c6bc65e9
gtajuve <gtajuve@gmail.com>
2016-03-21 23:38:13 +0200

    change view
    gallery

--- common/views/front/team/gallery.php
+++ common/views/front/team/gallery.php
@@ -10,10 +10,11 @@
     <div class="container">
         <div class="row">
             <?php foreach($images as $image): ?>
-                <div class="span3 ">
+                <div class="col-md-4">
 
-                    <?php if($image->getTitle()!='') { ?><a href="#" class="btn btn-mini btn-success "><?php echo $image->getTitle(); ?></a><?php } ?>
+
                     <img style="width:320px; height:220px;" class="img-responsive" src="admin/uploads/teams/images/<?php echo  $image->getImageName(); ?>" />
+                    <?php if($image->getTitle()!='') { ?><?php echo $image->getTitle(); ?><?php }else { echo 'No title';} ?>
                 </div>
             <?php endforeach; ?>
 
